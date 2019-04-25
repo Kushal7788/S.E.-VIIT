04225a3f407bf1e571d76f13ba935f9c790a6f16
Kushal <kushshah777888@gmail.com>
2019-04-25 10:15:17 +0530

    Hilbert and scan line

--- cg/ScanLine.java
+++ cg/ScanLine.java
@@ -3,81 +3,212 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package javaapplication1;
 import java.awt.*;
-import static java.awt.Color.black;
-import static java.awt.Color.red;
+import javax.swing.*;
+import java.lang.*;
 import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JFrame;
 /**
  *
- * @author kushal
+ * @author RohitL
  */
-public class ScanLine extends JFrame {
-     public void paint (Graphics g)
+public class ScanLine extends JFrame{
+    int no_vert = 5;
+    int [][] vert = new int[no_vert][4];
+    double [] slope = new double [no_vert];
+    Vector x_inter = new Vector();
+    /**
+     * @param args the command line arguments
+     */
+     public void delay()
     {
-       Scanl(g);     
-    }
-     public void Scanl(Graphics g){
-         Scanner cin = new Scanner(System.in);
-         g.setColor(Color.BLUE);
-//        g.drawLine(100, 100, 300, 500);
-//        g.drawLine(300, 500, 500, 500);
-//        g.drawLine(500, 500, 100, 100);
-        
-        int ver = 5;
-        int m1,m2;
-        int[] x = new int[ver];
-        int[] y = new int[ver];
-        int[] m = new int[ver];
-        for(int i=0;i<ver;i++)
+        try
         {
-            x[i] = cin.nextInt();
-            y[i] = cin.nextInt();
+            Thread.sleep(2);
         }
-        for(int i=0;i<ver;i++)
-        {
-            if (i == ver-1)
-            {
-                g.drawLine(x[i], y[i], x[0], y[0]);
-            }
+        catch(Throwable e)
+                {
+                    System.out.println(e.getMessage());
+                }
+    }
+    
+    int in_or_out(int y,int y1,int y2)
+    {
+        if((y1-y)*(y2-y)>0)
+            return 1;
+        else
+            return 0;
+    }
+    int ret_y1(int y,int m,int b)
+    {
+        int temp;
+        temp = (y-b)/m;
+        return temp;
+    }
+    
+    public static void sortbyColumn(int arr[][], int col1,int col2, int col3) 
+    { 
+        Arrays.sort(arr, new Comparator<int[]>() { 
+            
+          @Override              
+          public int compare(final int[] entry1,  
+                             final int[] entry2) { 
+  
+            // To sort in descending order revert  
+            // the '>' Operator 
+            if (entry1[col1] < entry2[col1]) 
+                return 1; 
+            else if((entry1[col1] == entry2[col1])&&(entry1[col2] < entry2[col2]))
+                return 1;
+            else if((entry1[col1] == entry2[col1])&&(entry1[col2] == entry2[col2])&&(entry1[col3] > entry2[col3]))
+                return 1;
             else
+                return -1; 
+          } 
+        });  // End of function call sort(). 
+    }
+    
+    double inv_slope(int x1,int y1, int x2, int y2)
+    {
+        return Math.round(((double)x2-(double)x1)/((double)y2-(double)y1)*100.0)/100.0;
+        //return m;
+    }
+    
+    void arrange()
+    {
+        int i;
+        for(i=0;i<no_vert;i++)
+        {
+            if(vert[i][1]<vert[i][3])
             {
-            int a=x[i],b=x[i+1],c=y[i],d=y[i+1];
-            g.drawLine(a, c, b, d);
+                int temp;
+                temp = vert[i][1];
+                vert[i][1] = vert[i][3];
+                vert[i][3] = temp;
+                temp = vert[i][0];
+                vert[i][0] = vert[i][2];
+                vert[i][2] = temp;
             }
         }
-        m1=2;
-        m2=1;
-//        int yt,yma=500,ymi=100;
-//        yt=yma-1;
-//        while(yt>=ymi)
-//        {
-//            int x1,x2;
-//            x1 = (yt+100)/m1;
-//            x2 = (yt)/m2;
-//            g.setColor(red);
-//            g.drawLine(x1+1, yt, x2-1, yt);
-//            yt--;
-//            try 
-//         {
-//             Thread.sleep(15);
-//         } 
-//         catch(Throwable e) 
-//         {
-//         System.out.println(e.getMessage()); 
-//         }
-//        }
-     }
-     public static void main(String[] args) { 
-        // TODO code application logic here
-        ScanLine obj = new ScanLine();
-        obj.setTitle("ScanLine");
-        obj.setSize(1366,768);
-        obj.setVisible(true);
+    }
+    
+    public void paint(Graphics g)
+    {
+       /* g.setColor(Color.red);
+        g.drawLine(400, 400, 700, 700);
+        g.drawLine(400, 400, 100, 700);
+        g.drawLine(100,700,700,700);
+        
+        int m1 = 1;
+        int m2 = -1;
+        int i;
+        int temp1,temp2;
+        g.setColor(Color.GREEN);
+        for(i=400;i<700;i++)
+        {
+            temp1 = ret_y1(i,1,0);
+            temp2 = ret_y1(i,-1,800);
+            delay();
+            g.drawLine(temp1, i, temp2, i);
+        }*/
+        
+        
+        vert[0][0] = 50;
+        vert[0][1] = 50;
+        vert[1][0] = 150;
+        vert[1][1] = 250;
+        vert[2][0] = 50;
+        vert[2][1] = 450;
+        vert[3][0] = 10;
+        vert[3][1] = 100;
+//        vert[4][0] = 100;
+//        vert[4][1] = 900;
+//        
+//        vert[4][2] = 100;
+//        vert[4][3] = 100;
+        vert[0][2] = 150;
+        vert[0][3] = 250;
+        vert[1][2] = 50;
+        vert[1][3] = 450;
+        vert[2][2] = 10;
+        vert[2][3] = 100;
+        vert[3][2] = 50;
+        vert[3][3] = 50;
+        g.setColor(Color.BLUE);
+        int i;
+        for(i=0;i<no_vert;i++)
+        {
+            
+        }
+        arrange();
+        sortbyColumn(vert, 1,3,2);
+        /*System.out.println(vert[0][0]);
+        System.out.println(vert[0][1]);
+        System.out.println(vert[0][2]);
+        System.out.println(vert[0][3]);
+        double x = (double)(2/3);*/
+        for(i=0;i<no_vert;i++)
+        {
+            g.drawLine(vert[i][0],vert[i][1],vert[i][2],vert[i][3]);
+            System.out.println();
+            System.out.print(" "+vert[i][0]);
+            System.out.print(" "+vert[i][1]);
+            System.out.print(" "+vert[i][2]);
+            System.out.print(" "+vert[i][3]);
+            slope[i] = inv_slope(vert[i][0],vert[i][1],vert[i][2],vert[i][3]);
+            System.out.print(" "+slope[i]);
+        }
+        int y_max = vert[0][1];
+        int y_min = vert[4][3];
+        System.out.println(" "+y_max+" "+y_min );
+        System.out.println();
+        g.setColor(Color.GREEN);
+        
+        int j;
+        for(i = y_max-1;i>y_min;i--)
+        {
+            System.out.print(" "+i);
+            for(j=0;j<no_vert;j++)
+            {
+                //System.out.println("Hello");
+                
+                if(i<=vert[j][1]&&i>vert[j][3])
+                {
+                    int xinet;
+                    xinet = (int)((slope[j]*(i-vert[j][1]))+vert[j][0]);
+                    System.out.print(" "+xinet);
+                    //System.out.println("Hello");
+                    x_inter.add(xinet);
+                }
+                
+            }
+            System.out.println();
+            Collections.sort(x_inter);
+          //System.out.println(x_inter.size());
+            
+            int k;
+            if(!x_inter.isEmpty())
+            {
+            for(k=0;k<1;k++)
+            {
+                g.drawLine((int)x_inter.get(k),i ,(int) x_inter.get(k+1), i);
+                delay();
+            }
+            x_inter.clear();
+            }
+        }
+        
         
     }
+    
+    
+    public static void main(String[] args) {
+        ScanLine obj = new ScanLine();
+
+        obj.setTitle("Scan Fill");
+        obj.setSize(300, 500);
+        obj.setVisible(true);
+        //obj.setBackground(Color.GRAY);
+    }
+    
 }
